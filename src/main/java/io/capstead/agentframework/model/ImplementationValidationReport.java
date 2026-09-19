83924825303bee61546beca3f169f3d0666d77b5
satya-anguluri <arahanttsystems@gmail.com>
2026-09-19 00:10:25 -0400

    fix: preserve review comparison metadata

--- src/main/java/io/capstead/agentframework/model/ImplementationValidationReport.java
+++ src/main/java/io/capstead/agentframework/model/ImplementationValidationReport.java
@@ -1,4 +1,4 @@
 package io.capstead.agentframework.model;
 import java.util.List;
-public record ImplementationValidationReport(WorkItem workItem,List<GitChange> observedChanges,
+public record ImplementationValidationReport(WorkItem workItem,List<RepositoryComparison> repositoryComparisons,List<GitChange> observedChanges,
  List<RequirementCheck> requirementChecks,String decision,List<String> limitations) {}
