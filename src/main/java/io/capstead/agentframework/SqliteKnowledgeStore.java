271e4e0fef6f666e21c25662db9f0221812ce4e3
satya-anguluri <arahanttsystems@gmail.com>
2026-09-18 21:21:19 -0400

    fix: close related Jira result formatting

--- src/main/java/io/capstead/agentframework/SqliteKnowledgeStore.java
+++ src/main/java/io/capstead/agentframework/SqliteKnowledgeStore.java
@@ -245,7 +245,7 @@ final class SqliteKnowledgeStore implements AutoCloseable {
             try(ResultSet rs=ps.executeQuery()){while(rs.next())rows.add(
                 "%s | %s | %s | %s%n%s%nCurrent evidence: %s %s at %s".formatted(
                  rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),
-                 rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8))));
+                 rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8)));
             }
         }
         return rows;
